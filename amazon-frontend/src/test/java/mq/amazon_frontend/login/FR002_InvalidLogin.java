7bbdb211ebb2bb35aec0ee93044918075d8b9f09
minhazq <minhazq@gmail.com>
2016-07-25 21:03:41 -0400

    FR002_InvalidLogin.java added

---
+++ amazon-frontend/src/test/java/mq/amazon_frontend/login/FR002_InvalidLogin.java
@@ -0,0 +1,14 @@
+package mq.amazon_frontend.login;
+
+import org.testng.annotations.Test;
+
+import mq.amazon_frontend.framework.Scriptbase;
+
+public class FR002_InvalidLogin extends Scriptbase{
+
+	
+	@Test
+	public void invalidLoginTest(){
+		amazon().homePage().clickLogin().enterEmail("test");
+	}
+}
